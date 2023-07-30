52a12cc1b170a053a1e7d0446be4c4e21e684ef9
PARTHIV <saikiap518@gmail.com>
2023-07-30 12:05:31 +0530

    loops in cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+/*int main(){
+    size_t i{0};
+    for(i;i<10;++i){
+        std::cout<<"ji"<<std::endl;
+        std::cout<<i<<std::endl;
+    }
+   
+        
+        
+        
+    
+}*/
+int main(){
+    size_t  i {0};
+    const size_t limit{20};
+    while(i<limit){
+        std::cout <<i<<":"<<"ho" <<std::endl;
+        ++i;
+    }
+}
